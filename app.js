edc4e90cbde5b3e0030dcad63396c721675d4fdb
caredlav <caredlav@gmail.com>
2021-10-22 13:46:47 -0500

    agregue variable port al app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const port=process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,"./public")));
 app.use(express.static(path.join(__dirname,"./views")));
@@ -17,4 +18,4 @@ app.get("/login",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/login.html"))
 })
 
-app.listen(3000,()=>{console.log("Initiated in port 3000")});
+app.listen(port,()=>{console.log("Initiated in port "+port)});
